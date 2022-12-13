48eb87ffdd45f82586eb99974ab6543a0eeadc15
Kamil S <61392709+szmitek@users.noreply.github.com>
2022-12-13 21:46:51 +0100

    backend update

--- backend/.keystone/admin/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/events-list-items":["static\u002Fchunks\u002Fpages\u002Fevents-list-items.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fevents-list-items"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
