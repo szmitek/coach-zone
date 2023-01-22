0a00d6e7c22169d2fc0a23bd8936c253b894ea59
Kamil S <61392709+szmitek@users.noreply.github.com>
2023-01-23 00:39:45 +0100

    :construction:  Changed access

--- backend/schemas/Position.ts
+++ backend/schemas/Position.ts
@@ -1,22 +1,3 @@
-export const footballPositions = [
-    { label: 'QB', value: 'QB' },
-    { label: 'RB', value: 'RB' },
-    { label: 'WR', value: 'WR' },
-    { label: 'OL', value: 'OL' },
-    { label: 'DL', value: 'DL' },
-    { label: 'LB', value: 'LB' },
-    { label: 'DB', value: 'DB' },
-];
-
-export const basketballPositions = [
-    { label: 'PG', value: 'PG' },
-    { label: 'SG', value: 'SG' },
-    { label: 'SF', value: 'SF' },
-    { label: 'PF', value: 'PF' },
-    { label: 'C', value: 'C' },
-];
-
-
 import {relationship, text} from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 import {isSignedIn, rules} from "../access";
@@ -24,9 +5,9 @@ import {isSignedIn, rules} from "../access";
 export const Position = list({
     access: {
         create: isSignedIn,
-        read: rules.canTeam,
-        update: rules.canTeam,
-        delete: rules.canTeam,
+        read:   () => true,
+        update: rules.canManageExercises,
+        delete: rules.canManageExercises,
     },
     ui: {
         listView: {
