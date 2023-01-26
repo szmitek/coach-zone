ac5492a3bb7263df01d8c238ddbd5bc87500b489
Kamil S <61392709+szmitek@users.noreply.github.com>
2023-01-26 17:45:23 +0100

    :construction:  Added working sport categories. :bug:  Displaying every exercises after switching page, not displaying all exercises while All is selected

--- frontend/pages/exercises/index.js
+++ frontend/pages/exercises/index.js
@@ -1,17 +1,19 @@
 import { useRouter } from 'next/dist/client/router';
 import Exercises from '../../components/Exercises';
 import Pagination from '../../components/Pagination';
-import SportCategories from "../../components/SportCategory";
+import SportCategories from '../../components/SportCategory'
 
 export default function OrderPage() {
-  const { query } = useRouter();
-  const page = parseInt(query.page);
-  return (
-    <div>
-      <Pagination page={page || 1} />
-        <SportCategories page={page || 1} />
-      <Exercises page={page || 1} />
-      <Pagination page={page || 1} />
-    </div>
-  );
+    const { query } = useRouter();
+    const page = parseInt(query.page);
+    const sportCategory = query.sportCategory;
+
+    return (
+        <div>
+            <Pagination page={page || 1} />
+            <SportCategories selectedCategory={sportCategory} />
+            <Exercises page={page || 1} sportCategory={sportCategory} />
+            <Pagination page={page || 1} />
+        </div>
+    );
 }
