f33925d377dff9896cba4b0993612bb9aef9cae5
Kamil S <61392709+szmitek@users.noreply.github.com>
2023-02-15 13:57:57 +0100

    :memo:  Added comments

--- frontend/components/SportCategory.js
+++ frontend/components/SportCategory.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import gql from 'graphql-tag';
 import SportCategoryStyles from "./styles/SportCategoryStyles";
 
+// define a GraphQL query to fetch all sport categories
 const ALL_SPORT_CATEGORIES_QUERY = gql`
     query ALL_SPORT_CATEGORIES_QUERY {
         allSportCategories {
@@ -14,17 +15,25 @@ const ALL_SPORT_CATEGORIES_QUERY = gql`
 `;
 
 export default function SportCategories({selectedCategory}) {
+    // use the `useRouter` hook to access the router object
     const router = useRouter();
+
+    // use the `useState` hook to initialize the `selectedSportCategory` and `currentPage` state variables
     const [selectedSportCategory, setSelectedSportCategory] = useState(selectedCategory || 'All');
     const [currentPage, setCurrentPage] = useState(1);
 
+    // use the `useQuery` hook to execute the `ALL_SPORT_CATEGORIES_QUERY` and fetch the data
     const { data, error, loading } = useQuery(ALL_SPORT_CATEGORIES_QUERY);
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
 
+    // define a function to handle sport category changes
     const handleSportCategoryChange = async (event) => {
+        // update the `selectedSportCategory` state variable with the selected value
         setSelectedSportCategory(event.target.value);
+        // reset the `currentPage` state variable to 1
         setCurrentPage(1);
+        // use the router object to update the URL with the selected sport category
         await router.push({
             pathname: '/exercises',
             query: {
@@ -33,7 +42,7 @@ export default function SportCategories({selectedCategory}) {
         });
     };
 
-
+    // render the `SportCategoryStyles` component and a select element with all the available sport categories
     return (
         <SportCategoryStyles>
             <div>
