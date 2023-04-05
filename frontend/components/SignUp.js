f73d231d059049d50cf9cb20802353b69ccadf5b
Kamil S <61392709+szmitek@users.noreply.github.com>
2023-04-06 01:51:33 +0200

    :bug:  Fixed creating team for new user

--- frontend/components/SignUp.js
+++ frontend/components/SignUp.js
@@ -11,7 +11,11 @@ const SIGNUP_MUTATION = gql`
     $name: String!
     $password: String!
   ) {
-    createUser(data: { email: $email, name: $name, password: $password }) {
+    createUser(data: { 
+      email: $email, 
+      name: $name, 
+      password: $password,
+    }) {
       id
       email
       name
@@ -19,108 +23,72 @@ const SIGNUP_MUTATION = gql`
   }
 `;
 
-const CREATE_TEAM_MUTATION = gql`
-  mutation CREATE_TEAM_MUTATION($userId: ID!) {
-    createTeam(data: { user: { connect: { id: $userId } } }) {
-      id
-      user {
-        id
-      }
-    }
-  }
-`;
-
 export default function SignUp() {
-  const [
-    signup,
-    { data: signupData, loading: signupLoading, error: signupError },
-  ] = useMutation(SIGNUP_MUTATION, {
-    refetchQueries: [{ query: CURRENT_USER_QUERY }],
-  });
-  const [
-    createTeam,
-    {
-      data: createTeamData,
-      loading: createTeamLoading,
-      error: createTeamError,
-    },
-  ] = useMutation(CREATE_TEAM_MUTATION);
   const { inputs, handleChange, resetForm } = useForm({
     email: '',
     name: '',
     password: '',
   });
-
+  const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION, {
+    variables: inputs,
+    // refectch the currently logged in user
+    // refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
   async function handleSubmit(e) {
-    e.preventDefault();
-    const res = await signup({
-      variables: {
-        email: inputs.email,
-        name: inputs.name,
-        password: inputs.password,
-      },
-    });
-
-    // If the signup is successful, create a new team and associate it with the logged in user
-    if (res.data.createUser) {
-      await createTeam({
-        variables: {
-          userId: res.data.createUser.id,
-        },
-      });
-    }
-
+    e.preventDefault(); // stop the form from submitting
+    console.log(inputs);
+    const res = await signup().catch(console.error);
+    console.log(res);
+    console.log({ data, loading, error });
     resetForm();
+    // Send the email and password to the graphqlAPI
   }
 
   return (
-    <Form method="POST" onSubmit={handleSubmit}>
-      <h2>Sign Up for an Account</h2>
-
-      <fieldset
-        disabled={signupLoading || createTeamLoading}
-        aria-busy={signupLoading || createTeamLoading}
-      >
-        {signupData?.createUser && (
-          <p>
-            Signed up with {signupData.createUser.email} - Please Go Head and
-            Sign in!
-          </p>
-        )}
-        <label htmlFor="email">
-          Your Name
-          <input
-            type="text"
-            name="name"
-            placeholder="Your Name"
-            autoComplete="name"
-            value={inputs.name}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="email">
-          Email
-          <input
-            type="email"
-            name="email"
-            placeholder="Your Email Address"
-            autoComplete="email"
-            value={inputs.email}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="password">
-          Password
-          <input
-            type="password"
-            name="password"
-            placeholder="Your Password"
-            value={inputs.password}
-            onChange={handleChange}
-          />
-        </label>
-        <button type="submit">Sign Up</button>
-      </fieldset>
-    </Form>
+      <Form method="POST" onSubmit={handleSubmit}>
+        <h2>Sign Up For an Account</h2>
+        <Error error={error} />
+        <fieldset>
+          {data?.createUser && (
+              <p>
+                Signed up with {data.createUser.email} - Please Go Head and Sign in!
+              </p>
+          )}
+          <label htmlFor="email">
+            Your Name
+            <input
+                type="text"
+                name="name"
+                placeholder="Your Name"
+                autoComplete="name"
+                value={inputs.name}
+                onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="email">
+            Email
+            <input
+                type="email"
+                name="email"
+                placeholder="Your Email Address"
+                autoComplete="email"
+                value={inputs.email}
+                onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                autoComplete="password"
+                value={inputs.password}
+                onChange={handleChange}
+            />
+          </label>
+          <button type="submit">Sign In!</button>
+        </fieldset>
+      </Form>
   );
 }
--- frontend/pages/team/[id].js
+++ frontend/pages/team/[id].js
@@ -7,61 +7,61 @@ import TeamStyles from '../../components/styles/TeamStyles';
 import { useUser } from '../../components/User';
 
 const TEAM_PLAYERS_QUERY = gql`
-  query TEAM_PLAYERS_QUERY($teamId: ID!) {
-    Team(where: { id: $teamId }) {
-      players(where: { team: { id: $teamId } }) {
-        id
-        name
-        position
-        number
-        description
-        strengths
-        weaknesses
-        photo {
-          image {
-            publicUrlTransformed
-          }
+    query TEAM_PLAYERS_QUERY($teamId: ID!) {
+        Team(where: { id: $teamId }) {
+            players(where: { team: { id: $teamId } }) {
+                id
+                name
+                position
+                number
+                description
+                strengths
+                weaknesses
+                photo {
+                    image {
+                        publicUrlTransformed
+                    }
+                }
+            }
         }
-      }
     }
-  }
 `;
 
 export default function ShowTeamPage() {
-  const me = useUser();
-  if (!me) return null;
+    const me = useUser();
+    if (!me) return null;
 
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { data, loading, error } = useQuery(TEAM_PLAYERS_QUERY, {
-    variables: { teamId: me.team.id },
-  });
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { data, loading, error } = useQuery(TEAM_PLAYERS_QUERY, {
+        variables: { teamId: me.team.id },
+    });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <ErrorMessage error={error} />;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <ErrorMessage error={error} />;
 
-  return (
-    <TeamStyles>
-      <Head>
-        <title>Your SQUAD</title>
-      </Head>
-      <div className="players">
-        {data.Team.players.map((teamItem) => (
-          <div className="team-player" key={teamItem.id}>
-            <img
-              src={teamItem.photo.image.publicUrlTransformed}
-              alt={teamItem.name}
-            />
-            <div className="player-details">
-              <h2>Name: {teamItem.name}</h2>
-              <h3>Position: {teamItem.position}</h3>
-              <h3>Number: {teamItem.number}</h3>
-              <p>About: {teamItem.description}</p>
-              <p>Strengths: {teamItem.strengths}</p>
-              <p>Weaknesses: {teamItem.weaknesses}</p>
+    return (
+        <TeamStyles>
+            <Head>
+                <title>Your SQUAD</title>
+            </Head>
+            <div className="players">
+                {data.Team.players.map((teamItem) => (
+                    <div className="team-player" key={teamItem.id}>
+                        <img
+                            src={teamItem.photo.image.publicUrlTransformed}
+                            alt={teamItem.name}
+                        />
+                        <div className="player-details">
+                            <h2>Name: {teamItem.name}</h2>
+                            <h3>Position: {teamItem.position}</h3>
+                            <h3>Number: {teamItem.number}</h3>
+                            <p>About: {teamItem.description}</p>
+                            <p>Strengths: {teamItem.strengths}</p>
+                            <p>Weaknesses: {teamItem.weaknesses}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-    </TeamStyles>
-  );
+        </TeamStyles>
+    );
 }
